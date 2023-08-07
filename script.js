f900a95b0b13ffc520bd33406f87c796e69c8e36
Crumunt <lorenz.bocatot@clsu2.edu.ph>
2023-08-07 15:27:35 +0800

    FINISED ADDING NECESSARY FUNCTIONS AND DESIGNS
    Added all the functions that will be used to make the game work and
    cleaned up the code (at least by my standards it is clean),I also
    finised the design for the buttons(added transform scale) and text.

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
-const score = document.querySelector('.scores');
-const playerScore = document.createElement('p');
-const computerScore = document.createElement('p');
+const moveButtons = document.querySelectorAll('button');
 
-const choices = document.querySelector('.choices');
-const playerChoice = document.createElement('p');
-const computerChoice = document.createElement('p');
-const winner = document.createElement('p');
-
-const buttons = document.querySelectorAll('button');
-
-buttons.forEach((button) => {
+moveButtons.forEach((button) => {
   button.addEventListener('click', () => {
     userMove = button.value;
+    computerMove = getComputerMove();
     updateUI();
   });
 });
 
-let player = 0;
-let computer = 0;
+const scoreDiv = document.querySelector('.scores');
+
+const playerScore = document.createElement('p');
+
+let currentPlayerScore = 0;
+playerScore.classList.add('player');
+playerScore.textContent = `Player Score: ${currentPlayerScore}`;
+
+const computerScore = document.createElement('p');
+
+let currentComputerScore = 0;
+computerScore.classList.add('computer');
+computerScore.textContent = `Computer Score: ${currentComputerScore}`;
+
+const choicesDiv = document.querySelector('.choices');
+const playerChoice = document.createElement('p');
+const computerChoice = document.createElement('p');
 
 let userMove = '';
 let computerMove = '';
-
-playerScore.classList.add('player');
-playerScore.textContent = `Player Score: ${player}`;
-
-computerScore.classList.add('computer');
-computerScore.textContent = `Computer Score: ${computer}`;
-
-score.appendChild(playerScore);
-score.appendChild(computerScore);
 
 playerChoice.classList.add('userChoice');
 playerChoice.textContent = `Player: ${userMove}`;
@@ -37,46 +35,114 @@ playerChoice.textContent = `Player: ${userMove}`;
 computerChoice.classList.add('aiChoice');
 computerChoice.textContent = `Computer: ${computerMove}`;
 
-choices.append(playerChoice);
-choices.append(computerChoice);
-choices.append(winner);
+const gameWinner = document.createElement('p');
+gameWinner.textContent = '';
+
+appendToHTML();
 
 function checkWinner() {
+
   if (userMove === computerMove) {
     return 'draw';
   } else if (userMove === 'rock' && computerMove !== 'paper') {
-    player++;
+    currentPlayerScore++;
     return 'Player Wins!';
   } else if (userMove === 'paper' && computerMove !== 'scissors') {
-    player++;
+    currentPlayerScore++;
     return 'Player Wins!'
   } else if (userMove === 'scissors' && computerMove !== 'rock') {
-    player++;
+    currentPlayerScore++;
     return 'Player Wins!';
   } else {
-    computer++;
+    currentComputerScore++;
     return 'Computer Wins!';
   }
 }
 
 function getComputerMove() {
-  const choice = Math.floor(Math.random() * 3) + 1;
+  const generateRandomNumber = Math.floor(Math.random() * 3) + 1;
 
-  if (choice == 1) {
-    return 'rock';
-  } else if (choice == 2) {
-    return 'paper';
-  } else {
-    return 'scissors';
+  switch(generateRandomNumber) {
+    case 1:
+      return 'rock';
+    case 2:
+      return 'paper';
+    case 3:
+      return 'scissors';
   }
 }
 
 function updateUI() {
-  computerMove = getComputerMove();
+
   playerChoice.textContent = `Player: ${userMove}`;
   computerChoice.textContent = `Computer: ${computerMove}`;
-  winner.textContent = checkWinner();
-  playerScore.textContent = `Player Score: ${player}`;
-  computerScore.textContent = `Computer Score: ${computer}`;
+
+  gameWinner.textContent = checkWinner();
+
+  playerScore.textContent = `Player Score: ${currentPlayerScore}`;
+  computerScore.textContent = `Computer Score: ${currentComputerScore}`;
+
+  if (currentPlayerScore === 5 || currentComputerScore === 5) {
+
+    const winnderDiv = document.querySelector('.gw');
+
+    winnderDiv.append(resetButton);
+
+    moveButtons.forEach((button) => {
+      button.disabled = true;
+    });
+
+    showWinner();
+  }
 }
 
+const resetButton = document.createElement('button');
+
+resetButton.textContent = 'Reset';
+
+resetButton.addEventListener('click', resetGame);
+
+function resetGame() {
+  currentPlayerScore = 0;
+  currentComputerScore = 0
+
+  userMove = '';
+  computerMove = '';
+
+  playerScore.textContent = `Player Score: ${currentPlayerScore}`;
+  computerScore.textContent = `Computer Score: ${currentComputerScore}`;
+
+  playerChoice.textContent = `Player: ${userMove}`;
+  computerChoice.textContent = `Computer: ${computerMove}`;
+
+  gameWinner.textContent = '';
+
+  const winnerDiv = document.querySelector('.gw');
+
+  winnerDiv.removeChild(resetButton);
+
+  moveButtons.forEach((button) => {
+    button.disabled = false;
+  });
+}
+
+function appendToHTML() {
+
+  const winnerDiv = document.querySelector('.gw');
+
+  winnerDiv.append(gameWinner);
+
+  scoreDiv.appendChild(playerScore);
+  scoreDiv.appendChild(computerScore);
+
+  choicesDiv.append(playerChoice);
+  choicesDiv.append(computerChoice);
+}
+
+function showWinner() {
+  if(currentPlayerScore === 5) {
+    gameWinner.textContent = 'Player has won the match!';
+  }else {
+    gameWinner.textContent = 'Computer has won the match :<';
+  }
+}
